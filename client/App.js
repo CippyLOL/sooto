2a8bbca8e9c77b2f4369929b680ab585754450e4
Gillianne Papasin <47998428+CippyLOL@users.noreply.github.com>
2022-06-06 20:53:22 +0800

    Added react navigation

--- client/App.js
+++ client/App.js
@@ -1,12 +1,19 @@
 import React from "react";
-import { NativeBaseProvider, Box } from "native-base";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NativeBaseProvider } from "native-base";
+import SignUpScreen from "./screens/SignUpScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <Box flex={1} justifyContent="center" alignItems="center">
-        Hello world
-      </Box>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Sign Up" component={SignUpScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </NativeBaseProvider>
   );
 }
---
+++ client/screens/SignUpScreen.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View } from "react-native";
+import { Box, Text } from "native-base";
+
+const SignUpScreen = (props) => {
+  return (
+    <Box flex={1} justifyContent="center" alignItems="center">
+      <Text fontSize="2xl">Sign Up Screen</Text>
+      <Text fontSize="sm">This is a desc</Text>
+    </Box>
+  );
+};
+
+export default SignUpScreen;
